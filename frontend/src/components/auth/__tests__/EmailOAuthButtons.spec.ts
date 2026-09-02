CraftByte-arch/sub2api

fix: preserve promo codes for OAuth signup

frontend/src/components/auth/__tests__/EmailOAuthButtons.spec.ts
@@ -33,6 +33,7 @@ describe('EmailOAuthButtons', () => {
       props: {
         githubEnabled: true,
         googleEnabled: false,
+        promoCode: ' PROMO123 ',
       },
       global: {
         stubs: {
@@ -47,7 +48,7 @@ describe('EmailOAuthButtons', () => {
     expect(wrapper.emitted('start')).toEqual([[
       {
         provider: 'github',
-        params: { redirect: '/billing?plan=pro', aff_code: 'AFF123' }
+        params: { redirect: '/billing?plan=pro', aff_code: 'AFF123', promo_code: 'PROMO123' }
       }
     ]])
     expect(window.sessionStorage.getItem('oauth_aff_code')).toBe('AFF123')
@@ -60,6 +61,7 @@ describe('EmailOAuthButtons', () => {
       props: {
         githubEnabled: false,
         googleEnabled: true,
+        promoCode: 'PROMO456',
       },
       global: {
         stubs: {
@@ -73,7 +75,7 @@ describe('EmailOAuthButtons', () => {
 
     expect(wrapper.emitted('start')?.[0]?.[0]).toEqual({
       provider: 'google',
-      params: { redirect: '/billing?plan=pro', aff_code: 'AFF123' }
+      params: { redirect: '/billing?plan=pro', aff_code: 'AFF123', promo_code: 'PROMO456' }
     })
     expect(window.location.href).toBe(originalHref)
   })

Cfl-scxr/TokenRouter

frontend/src/components/auth/__tests__/EmailOAuthButtons.spec.ts
@@ -33,6 +33,7 @@ describe('EmailOAuthButtons', () => {
       props: {
         githubEnabled: true,
         googleEnabled: false,
+        promoCode: ' PROMO123 ',
       },
       global: {
         stubs: {
@@ -47,7 +48,7 @@ describe('EmailOAuthButtons', () => {
     expect(wrapper.emitted('start')).toEqual([[
       {
         provider: 'github',
-        params: { redirect: '/billing?plan=pro', aff_code: 'REF123' }
+        params: { redirect: '/billing?plan=pro', aff_code: 'REF123', promo_code: 'PROMO123' }
       }
     ]])
     expect(window.sessionStorage.getItem('oauth_aff_code')).toBe('REF123')
@@ -60,6 +61,7 @@ describe('EmailOAuthButtons', () => {
       props: {
         githubEnabled: false,
         googleEnabled: true,
+        promoCode: 'PROMO456',
       },
       global: {
         stubs: {
@@ -73,7 +75,7 @@ describe('EmailOAuthButtons', () => {
 
     expect(wrapper.emitted('start')?.[0]?.[0]).toEqual({
       provider: 'google',
-      params: { redirect: '/billing?plan=pro', aff_code: 'REF123' }
+      params: { redirect: '/billing?plan=pro', aff_code: 'REF123', promo_code: 'PROMO456' }
     })
     expect(window.location.href).toBe(originalHref)
   })